f554ff26b8725153931f697ab8d69597368a8f8c
Hank Star <kate.stapley@hotmail.com>
2024-10-14 19:16:01 +1100

    Monday class commits

--- 02-Stu_Create-Vite/Unsolved/src/App.jsx
+++ 02-Stu_Create-Vite/Unsolved/src/App.jsx
@@ -1,7 +1,10 @@
 // TODO: Add a comment explaining what this import statement is doing
+// The import statement is importing the HelloReact component from the HelloReact.js file in the components folder. 
 import HelloReact from './components/HelloReact';
 
 // TODO: Add a comment explaining the purpose of the App component
+// The App component is the main component that is being rendered to the DOM. 
+// It is the parent component that will render the HelloReact component.
 function App() {
   return <HelloReact />;
 }
--- 10-Stu_Components/Unsolved/src/components/List.jsx
+++ 10-Stu_Components/Unsolved/src/components/List.jsx
@@ -2,10 +2,12 @@ function List(props) {
   return (
     <ul className="list-group">
       {/* TODO: Use the `map` method to iterate over each grocery and return a `li` element that contains the item's name and has a unique key attribute */}
-      {props.groceries.map((item) => {
+      {props.groceries.map((item, index) => {
         return (
           // TODO: Your code here
-          <li></li>
+          <li key={index} className="list-group-item">
+            {item.name}
+          </li>
         );
       })}
     </ul>
