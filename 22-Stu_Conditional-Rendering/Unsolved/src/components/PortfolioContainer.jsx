fe41af2fad0b2ea8b6198abeffe45cec435e3e9d
Hank Star <kate.stapley@hotmail.com>
2024-10-15 20:03:11 +1100

    task 22 and 24

--- 22-Stu_Conditional-Rendering/Unsolved/src/components/PortfolioContainer.jsx
+++ 22-Stu_Conditional-Rendering/Unsolved/src/components/PortfolioContainer.jsx
@@ -9,6 +9,12 @@ export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // TODO: Add a comment describing the functionality of this method
+  // This method is rendering the page based on the currentPage state.
+  // If the currentPage is 'Home' then it renders the Home component.
+  // If the currentPage is 'About' then it renders the About component.
+  // If the currentPage is 'Blog' then it renders the Blog component.
+  // If the currentPage is 'Contact' then it renders the Contact component.
+
   const renderPage = () => {
     if (currentPage === 'Home') {
       return <Home />;
@@ -27,8 +33,17 @@ export default function PortfolioContainer() {
   return (
     <div>
       {/* // TODO: Add a comment describing what we are passing as props */}
+      // We are passing the currentPage state and the handlePageChange method as props.
+      // The currentPage state is used to determine which page to render.
+      // The handlePageChange method is used to update the currentPage state.
+      // The NavTabs component uses the currentPage state to determine which tab is active.
+      // The NavTabs component uses the handlePageChange method to update the currentPage state when a tab is clicked.
+      // The renderPage method uses the currentPage state to determine which page to render.
+
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* // TODO: Add a comment explaining what is happening on the following line */}
+      // The renderPage method is called to render the page based on the currentPage state.
+      // The currentPage state is initially set to 'Home', so the Home component is rendered.
       <main className="mx-3">{renderPage()}</main>
     </div>
   );
--- 24-Stu_React-Router/Unsolved/src/App.jsx
+++ 24-Stu_React-Router/Unsolved/src/App.jsx
@@ -1,5 +1,5 @@
 // Todo: Bring in the required import from 'react-router-dom'
-import {  } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'; // Outlet renders the matching child route component
 import Nav from './components/Nav';
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
     <>
       <Nav />
       {/* Todo: Fill in the appropriate component so that the user will conditionally see the appropriate view based on the URL */}
+      <Outlet /> {/* This will render the child route component based on the current route */}
     </>
   );
 }
